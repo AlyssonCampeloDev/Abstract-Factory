7db843bf5f862e6e8c0a0077764155777fb6b223
Alysson Campelo <87710684+AlyssonCampeloDev@users.noreply.github.com>
2022-09-14 10:45:57 -0300

    Add files via upload

---
+++ prototype.java
@@ -0,0 +1,17 @@
+public abstract class Veiculo {
+        
+    public Veiculo(String modelo, String marca, String cor, int numeroRodas) {
+        
+        this.cor = preta;
+        this.modelo = civc;
+        this.marca = honda;
+        this.numeroRodas = 4;
+    }
+}
+public class Hilux extends Veiculo {
+    public Hilux (int numeroPortas)
+}
+public class Onibus extends Veiculo {
+    public Onibus (int numeroRodas)
+
+}
